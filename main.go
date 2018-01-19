4dccece36d8f8f57d34bf1173c1b0e7b4ab9978d
Thomas Desrosiers <thomas@databricks.com>
2018-01-19 00:28:53 -0800

    Batcher

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -43,28 +44,90 @@ func init() {
 	}
 }
 
+func bufferedLineSplitter(in io.Reader) chan []byte {
+	scanner := bufio.NewScanner(in)
+	out := make(chan []byte)
+
+	go func() {
+		defer close(out)
+
+		for scanner.Scan() {
+			rawBytes := scanner.Bytes()
+
+			buf := make([]byte, len(rawBytes))
+			copy(buf, rawBytes)
+
+			out <- buf
+		}
+
+		switch err := scanner.Err(); err {
+		case nil, io.EOF:
+			return
+		default:
+			log.Println(errors.Wrap(err, "scanner returned error"))
+		}
+	}()
+
+	return out
+}
+
+// Batch limit taken from https://docs.aws.amazon.com/firehose/latest/APIReference/API_PutRecordBatch.html
+const (
+	BATCH_RECORD_ITEM_LIMIT = 500
+	BATCH_RECORD_SIZE_LIMIT = 500
+)
+
+type recordBatch []*firehose.Record
+
+func (rb *recordBatch) add(r *firehose.Record) {
+	rb = append(rb, r)
+}
+
+func (rb *recordBatch) reset() recordBatch {
+	rb = make([]*firehose.Record, 0, BATCH_RECORD_ITEM_LIMIT)
+	return rb
+}
+
+func firehoseRecordBatcher(in chan []byte) chan []*firehose.Record {
+	out = make(chan []*firehose.Record)
+
+	recordBuf := make(recordBatch)
+	recordBuf.reset()
+
+	sizeCounter := 0
+
+	go func() {
+		for buf := range in {
+			if sizeCounter+len(buf) > BATCH_RECORD_SIZE_LIMIT {
+				out <- recordBuf
+				recordBuf.reset()
+			}
+
+			recordBuf.add(&firehose.Record{
+				Data: buf,
+			})
+		}
+	}()
+
+	return
+}
+
 func main() {
 	firehoseClient := firehose.New(session.Must(session.NewSession()))
 
-	scanner := bufio.NewScanner(input)
-	for scanner.Scan() {
-		buf := make([]byte, len(scanner.Bytes()))
-		copy(buf, scanner.Bytes())
+	lines := bufferedLineSplitter(input)
 
-		fpro, err := firehoseClient.PutRecord(&firehose.PutRecordInput{
+	for line := range lines {
+		_, err := firehoseClient.PutRecord(&firehose.PutRecordInput{
 			DeliveryStreamName: kinesisStreamName,
 			Record: &firehose.Record{
-				Data: buf,
+				Data: line,
 			},
 		})
 		if err != nil {
 			log.Println(errors.Wrap(err, "failed to PutRecord"))
 		}
 
-		log.Println(fpro.String())
-	}
-
-	if scanner.Err() != nil {
-		log.Fatal(errors.Wrap(scanner.Err(), "line scanner failed"))
+		fmt.Print(".")
 	}
 }
