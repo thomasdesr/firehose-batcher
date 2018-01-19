46c6cbff124745f5be0814ba1e89047568919449
Thomas Desrosiers <thomas@databricks.com>
2018-01-19 02:20:51 -0800

    Better Batcher

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/firehose"
@@ -73,59 +74,127 @@ func bufferedLineSplitter(in io.Reader) chan []byte {
 
 // Batch limit taken from https://docs.aws.amazon.com/firehose/latest/APIReference/API_PutRecordBatch.html
 const (
-	BATCH_RECORD_ITEM_LIMIT = 500
-	BATCH_RECORD_SIZE_LIMIT = 500
+	BATCH_ITEM_LIMIT = 500
+	BATCH_SIZE_LIMIT = 4 * 1024 * 1024 // 4MB
+
+	PER_ITEM_SIZE_LIMIT = 1024 * 1024 // 1 MB
 )
 
-type recordBatch []*firehose.Record
+type FirehoseBatcher struct {
+	maxSendInterval time.Duration
 
-func (rb *recordBatch) add(r *firehose.Record) {
-	rb = append(rb, r)
+	firehoseClient *firehose.Firehose
+
+	inputBuffer chan []byte
+	closed      bool
 }
 
-func (rb *recordBatch) reset() recordBatch {
-	rb = make([]*firehose.Record, 0, BATCH_RECORD_ITEM_LIMIT)
-	return rb
+func NewFirehoseBatcher(fc *firehose.Firehose, sendInterval time.Duration) (*FirehoseBatcher, error) {
+	fb := &FirehoseBatcher{
+		maxSendInterval: sendInterval,
+		firehoseClient:  fc,
+
+		inputBuffer: make(chan []byte, BATCH_ITEM_LIMIT),
+	}
+
+	return fb, nil
 }
 
-func firehoseRecordBatcher(in chan []byte) chan []*firehose.Record {
-	out = make(chan []*firehose.Record)
+func (fb *FirehoseBatcher) Add(msg []byte) error {
+	if len(msg) > PER_ITEM_SIZE_LIMIT {
+		return errors.New("item exceeds firehose's max item size")
+	}
 
-	recordBuf := make(recordBatch)
-	recordBuf.reset()
+	fb.inputBuffer <- msg
+	return nil
+}
 
-	sizeCounter := 0
+func (fb *FirehoseBatcher) Start(streamName string) error {
+	var overflow *firehose.Record
+	for stop := false; !stop; {
+		buf := make([]*firehose.Record, 0, BATCH_ITEM_LIMIT)
+		bufSize := 0
 
-	go func() {
-		for buf := range in {
-			if sizeCounter+len(buf) > BATCH_RECORD_SIZE_LIMIT {
-				out <- recordBuf
-				recordBuf.reset()
-			}
-
-			recordBuf.add(&firehose.Record{
-				Data: buf,
-			})
+		if overflow == nil {
+			buf = append(buf, &firehose.Record{Data: <-fb.inputBuffer})
+		} else {
+			buf = append(buf, overflow)
+			overflow = nil
 		}
-	}()
 
-	return
+	BatchingLoop:
+		for i := 0; i < BATCH_ITEM_LIMIT-1; i++ {
+			select {
+			case <-time.After(fb.maxSendInterval):
+				break BatchingLoop
+
+			case item, ok := <-fb.inputBuffer:
+				if !ok {
+					// Input channel is closed, we're done here exit the batching loop, send the last batch, and return
+					stop = true
+					break BatchingLoop
+				}
+
+				record := &firehose.Record{Data: item}
+				if bufSize+len(item) > BATCH_SIZE_LIMIT {
+					// We're gonna overflow a batch, save the current one and end the batch early
+					overflow = record
+					break BatchingLoop
+				}
+
+				buf = append(buf, record)
+				bufSize += len(item)
+			}
+		}
+
+		if err := fb.sendBatch(streamName, buf); err != nil {
+			return errors.Wrap(err, "error sending batch")
+		}
+	}
+
+	return nil
+}
+
+func (fb *FirehoseBatcher) sendBatch(streamName string, batch []*firehose.Record) error {
+	prbo, err := fb.firehoseClient.PutRecordBatch(&firehose.PutRecordBatchInput{
+		DeliveryStreamName: &streamName,
+
+		Records: batch,
+	})
+
+	if err != nil {
+		return err
+	} else if *prbo.FailedPutCount > 0 {
+		return errors.Errorf(
+			"failed to send the full batch (%d failed), retries not currently handled",
+			*prbo.FailedPutCount,
+		)
+	}
+
+	return nil
 }
 
 func main() {
 	firehoseClient := firehose.New(session.Must(session.NewSession()))
 
-	lines := bufferedLineSplitter(input)
+	firehoseStreamName := "osquery-fleet-test"
 
-	for line := range lines {
-		_, err := firehoseClient.PutRecord(&firehose.PutRecordInput{
-			DeliveryStreamName: kinesisStreamName,
-			Record: &firehose.Record{
-				Data: line,
-			},
-		})
+	batcher, err := NewFirehoseBatcher(firehoseClient, time.Second*60)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "failed to create firehose batcher"))
+	}
+
+	go func() {
+		err := batcher.Start(firehoseStreamName)
 		if err != nil {
-			log.Println(errors.Wrap(err, "failed to PutRecord"))
+			log.Fatal(errors.Wrap(err, "batch sending exited early"))
+		}
+	}()
+
+	lines := bufferedLineSplitter(input)
+	for line := range lines {
+		if err := batcher.Add(line); err != nil {
+			log.Println(errors.Wrap(err, "failed to add record to batcher"))
 		}
 
 		fmt.Print(".")
